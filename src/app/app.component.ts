2f2d733c515592b5fac5023f07232dcf49e170b6
Luis Alberto <lmarincarmona@safareyes.es>
2024-10-24 19:00:18 +0200

    probando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PruebaComponent} from '../componentes/prueba/prueba.component';
+import {ListaComponent} from '../componentes/lista/lista.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PruebaComponent, ListaComponent, BrowserAnimationsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
