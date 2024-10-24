bc6570ed65f14c4f99abec6ec11cd560b79c8a75
Luis Alberto <lmarincarmona@safareyes.es>
2024-10-24 20:06:36 +0200

    funcionando los animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {PruebaComponent} from '../componentes/prueba/prueba.component';
 import {ListaComponent} from '../componentes/lista/lista.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PruebaComponent, ListaComponent, BrowserAnimationsModule],
+  imports: [RouterOutlet, PruebaComponent, ListaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
