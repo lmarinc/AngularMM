afcf8c037f90f7f088069dd6c57a06733ea3af6b
Luis Alberto <lmarincarmona@safareyes.es>
2024-11-04 18:34:12 +0100

    con tail-wind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {PruebaComponent} from '../componentes/prueba/prueba.component';
-import {ListaComponent} from '../componentes/lista/lista.component';
-import {AnimacionComponent} from '../componentes/animacion/animacion.component';
-import {NotasComponent} from '../componentes/notas/notas.component';
-
+import { PruebaComponent } from '../componentes/prueba/prueba.component';
+import { ListaComponent } from '../componentes/lista/lista.component';
+import { AnimacionComponent } from '../componentes/animacion/animacion.component';
+import { NotasComponent } from '../componentes/notas/notas.component';
+import { GaleriaComponent } from '../componentes/galeria/galeria.component';
+import { TareasComponent } from '../componentes/tareas/tareas.component';
+import { ImageModalComponent } from '../componentes/image-modal/image-modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PruebaComponent, ListaComponent, AnimacionComponent, NotasComponent],
+  imports: [
+    RouterOutlet,
+    PruebaComponent,
+    ListaComponent,
+    AnimacionComponent,
+    NotasComponent,
+    GaleriaComponent,
+    TareasComponent,
+    ImageModalComponent // Asegúrate de incluir otros componentes necesarios aquí
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Asegúrate de que esta línea esté correcta
 })
 export class AppComponent {
   title = 'AngularMM';
